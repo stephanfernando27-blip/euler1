b79260eb93d54d7356592549c6ee02fff6d0f684
stephanfernando27-blip <stephanfernando27@gmail.com>
2025-10-06 21:00:50 +0530

    Create euler_7.py

---
+++ euler_7.py
@@ -0,0 +1,15 @@
+def is_prime(n):
+    if n < 2: return False
+    if n % 2 == 0 and n != 2: return False
+    for i in range(3, int(n**0.5)+1, 2):
+        if n % i == 0:
+            return False
+    return True
+
+count, num = 0, 1
+while count < 10001:
+    num += 1
+    if is_prime(num):
+        count += 1
+
+print(num)
